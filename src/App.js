5650c4a8ba29a8670e14aa9d957201af7d8ae96a
akash plackal <akashplackal@gmail.com>
2022-09-22 19:47:38 +0530

    timer 50% done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import LoginScreen from './components/LoginScreen.js'
 import Timer from './components/Timer.js'
 
 
 const App = () => {
 
-  const [isLoged, setIsLoged] = useState(
-    () => localStorage.getItem('email')
-  )
+  const [isLoged, setIsLoged] = useState(false)
+
+  useEffect(() => {
+    if (localStorage.getItem('email')) {
+      setIsLoged(true)
+    }
+  }, [isLoged])
+
 
   return (
     <>
